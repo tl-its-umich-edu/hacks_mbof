1732ee721a5f76910e54abd39ecdfa575f94a7d0
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-02-20 11:50:59 -0500

    Very important to use 0.0.0.0, because Vagrant can't forward ports from localhost.

--- manage.py
+++ manage.py
@@ -7,4 +7,10 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    try:
+        if sys.argv[1] == "runserver" and len(sys.argv) == 2:
+            sys.argv.append('0.0.0.0:8000')
+    except:
+        pass
+
     execute_from_command_line(sys.argv)
