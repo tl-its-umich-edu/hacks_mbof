7c71a21b703e93494c4feaf8ceb10aca69466bad
Gonzalo Silverio <gsilver@umich.edu>
2016-03-03 14:37:01 -0500

    display_bofs - rough outline of displaying all bofs

--- mbofui/app/js/controllers.js
+++ mbofui/app/js/controllers.js
@@ -3,13 +3,65 @@
 
 mbofuiApp.controller('mbofuiAppController', ['$scope', 'Bof', '$log', '$window', function($scope, Bof, $log, $window) {
 
+
+    var pos = $window.navigator.geolocation.getCurrentPosition(posSuccess, posError);
+
+    function posSuccess(pos) {
+        var latlng = new google.maps.LatLng(pos.coords.latitude,
+            pos.coords.longitude);
+        var options = {
+            zoom: 18,
+            center: latlng,
+            mapTypeControl: true,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+        var map = new google.maps.Map(document.getElementById('map'), options);
+
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map
+        });
+        getBofs(map);
+    }
+
+    function posError(map) {
+        var map = new google.maps.Map(document.getElementById('map'), options);
+        getBofs(map);
+    }
+
+    function getBofs(map) {
+        var bofsUrl = '/api/messages/'
+        Bof.GetBofs(bofsUrl).then(function(result) {
+            $scope.totalBofs = result.data.results.length;
+            angular.forEach(result.data.results, function(bof) {
+                var marker = new google.maps.Marker({
+                    position: { lat: bof.latitude, lng: bof.longitude },
+                    map: map
+                });
+                var iconFile = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'; 
+                marker.setIcon(iconFile) 
+                
+                google.maps.event.addListener(marker, 'click', function() {
+                    var infowindow = new google.maps.InfoWindow({
+                        content: bof.messageText
+                    });
+                    infowindow.open(map, marker);
+                });
+
+
+
+            });
+
+        });
+     }   
+    
     $scope.postBof = function() {
         var pos = $window.navigator.geolocation.getCurrentPosition(posSuccess, posError);
 
         function posSuccess(pos) {
             $scope.coords = pos.coords;
             // serialize the inputs to create a URL
-            var url = '/messages?messageText=' + $scope.newMessageText +
+            var url = '/api/messages?messageText=' + $scope.newMessageText +
                 '&startTime=' + $scope.newStartTime +
                 '&endTime=' + $scope.newEndTime +
                 '&latitude=' + $scope.coords.latitude +
--- mbofui/app/js/factories.js
+++ mbofui/app/js/factories.js
@@ -14,6 +14,21 @@ mbofuiApp.factory('Bof', function($q, $timeout, $window, $http) {
           function error(result) {
             // do some error things
           });
+    },
+    GetBofs : function(url) {
+      return $http.get(url, {
+        cache : false
+      }).then(
+          function success(result) {
+            // we will get a response;
+            // what to do with it
+            return result;
+          },
+          function error(result) {
+            // do some error things
+          });
     }
+
+
   };
 });
