fc61b29ee86c055e695fb27b7ec2b8b576ea9907
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-03-03 16:54:28 -0500

    Note done yet.  Weird errors.

--- mbof/serializers.py
+++ mbof/serializers.py
@@ -1,6 +1,10 @@
 import logging
 import os
+from curses import has_key
 
+from datetime import datetime, timedelta
+
+import dateutil.parser
 from rest_framework import serializers
 
 from .models import Message, User
@@ -24,4 +28,23 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
     def is_valid(self, raise_exception=False):
         # FIXME: This method of setting the owner URL seems hackish.  Find the right way.
         self.initial_data[u'owner'] = u'http://localhost:18000/api/users/%s/' % os.getenv('REMOTE_USER')
+
+
+        postingTime = datetime.utcnow().isoformat()
+        self.initial_data[u'postingTime'] = postingTime
+
+        startTime = postingTime
+        if (not has_key(self.initial_data, u'startTime')):
+            self.initial_data[u'startTime'] = startTime
+        else:
+            try:
+                startTime = dateutil.parser.parse(self.initial_data[u'startTime'])
+            except:
+                pass
+
+        if (not has_key(self.initial_data, u'endTime')):
+            endTime = startTime + timedelta(days=5)
+            self.initial_data[u'startTime'] = endTime
+
+
         return super(MessageSerializer, self).is_valid(raise_exception)
