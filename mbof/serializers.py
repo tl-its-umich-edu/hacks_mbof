4aa02fadc2ddf665587316e8a4d8fddf53585b23
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-03-04 08:31:47 -0500

    Not the appropriate place for altering incoming data.

--- mbof/serializers.py
+++ mbof/serializers.py
@@ -1,10 +1,5 @@
 import logging
-import os
-from curses import has_key
 
-from datetime import datetime, timedelta
-
-import dateutil.parser
 from rest_framework import serializers
 
 from .models import Message, User
@@ -24,27 +19,3 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
         model = Message
         fields = ('url', 'messageText', 'latitude', 'longitude', 'altitudeMeters', 'owner', 'postingTime', 'startTime',
                   'endTime',)
-
-    def is_valid(self, raise_exception=False):
-        # FIXME: This method of setting the owner URL seems hackish.  Find the right way.
-        self.initial_data[u'owner'] = u'http://localhost:18000/api/users/%s/' % os.getenv('REMOTE_USER')
-
-
-        postingTime = datetime.utcnow().isoformat()
-        self.initial_data[u'postingTime'] = postingTime
-
-        startTime = postingTime
-        if (not has_key(self.initial_data, u'startTime')):
-            self.initial_data[u'startTime'] = startTime
-        else:
-            try:
-                startTime = dateutil.parser.parse(self.initial_data[u'startTime'])
-            except:
-                pass
-
-        if (not has_key(self.initial_data, u'endTime')):
-            endTime = startTime + timedelta(days=5)
-            self.initial_data[u'startTime'] = endTime
-
-
-        return super(MessageSerializer, self).is_valid(raise_exception)
