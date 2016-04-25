cab8dd00d60ed2b9c936cedb2d489ecb2c116939
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-04-25 17:34:00 -0400

    Based on discussion with dovek, added example of registering a view that uses the same model class as another view.  When that happens, the view(s) need to have "base_name" specified.  This was also the problem with long-standing issue for the "CurrentUserViewSet".

--- hacks_mbof/urls.py
+++ hacks_mbof/urls.py
@@ -23,7 +23,8 @@ import mbof.urls
 from mbof import views
 
 router = routers.DefaultRouter()
-router.register(r'me', views.CurrentUserViewSet)
+router.register(r'me', views.CurrentUserViewSet, base_name='me')
+router.register(r'recent_messages', views.RecentMessageViewSet, base_name='recent_messages')
 router.register(r'messages', views.MessageViewSet)
 router.register(r'users', views.UserViewSet)
 router.register(r'votes', views.VoteViewSet)
