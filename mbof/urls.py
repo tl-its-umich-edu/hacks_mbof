1b804052b3acc4643b218650faf19e9b78b897ef
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-04-27 16:28:44 -0400

    Message becomes Event. Removed unneeded references to page templates.

--- mbof/urls.py
+++ mbof/urls.py
@@ -1,11 +1,2 @@
-from django.conf.urls import url
-
-from . import views
-
 app_name = 'mbof'
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<messageId>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<messageId>[0-9]+)/results/$', views.results, name='results'),
-    url(r'^(?P<messageId>[0-9]+)/vote/$', views.vote, name='vote'),
-]
+urlpatterns = []
